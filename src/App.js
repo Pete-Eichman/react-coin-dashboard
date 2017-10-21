f493e2e167f98ba3ed1105299ec97b1953a071d7
sammcgrail <sammcgrail@gmail.com>
2017-10-21 15:26:38 -0400

    example csv data coerced into format, components made

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Fetch from 'react-fetch'
 import TestComponent from './TestComponent'
+import ExampleChart from './ExampleChart'
 import './App.css';
 
 class App extends Component {
@@ -8,7 +9,10 @@ class App extends Component {
    return (
      <Fetch url="https://api.coinmarketcap.com/v1/ticker/bitcoin/">
        <TestComponent/>
+       <ExampleChart/>
      </Fetch>
+
+
    )
  }
 }
